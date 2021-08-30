fc2d0091b670f131245ca4e74db68d11eaa4191c
wellido <707376149@qq.com>
2021-08-30 12:42:03 +0200

    remove personal info

--- EGL_VGG19.sh
+++ EGL_VGG19.sh
@@ -1,19 +1,5 @@
 #!/bin/bash -l
 
-#SBATCH -n 2
-#SBATCH -N 1
-#SBATCH -p gpu
-#SBATCH --gres=gpu:1
-#SBATCH --time=0-40:00:00
-#SBATCH -C skylake
-#SBATCH -J eglvgg19
-#SBATCH --mail-type=end,fail
-#SBATCH --mail-user=qiang.hu@uni.lu
-
-module purge
-module load swenv/default-env/devel
-module load system/CUDA numlib/cuDNN
-
 for((i=0;i<25;i++));
 do
 python -u EGL_VGG19.py
--- EGL_wideresnet.sh
+++ EGL_wideresnet.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -l
 
-#SBATCH -n 10
-#SBATCH -N 2
-#SBATCH -p gpu
-#SBATCH --gres=gpu:2
-#SBATCH --time=0-40:00:00
-#SBATCH -C skylake
-#SBATCH -J wncc_2
-#SBATCH --mail-type=end,fail
-#SBATCH --mail-user=qiang.hu@uni.lu
-
-module purge
-module load swenv/default-env/devel
-module load system/CUDA numlib/cuDNN
 
 for((i=0;i<25;i++));
 do
